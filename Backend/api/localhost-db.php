41392ddde57b42fd994b0e377f25c908d741f342
folk prototype <6411130035@mut.ac.th>
2023-09-30 15:44:52 +0700

    commit

--- Backend/api/localhost-db.php
+++ Backend/api/localhost-db.php
@@ -2,7 +2,7 @@
 // MySQL database credentials
 $servername = "localhost";
 $username = "user01";
-$password = "1234";
+$password = "123";
 $dbname = "mini-project";
 
 // Establish a connection to the MySQL database
